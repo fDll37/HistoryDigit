ee97be25a7e3a0707f88e60ecd23dada02f0bc20
fDll37 <dmendelev@yandex.ru>
2023-02-07 22:26:23 +0300

    init

---
+++ HistoryDigit/View/Cabinet.swift
@@ -0,0 +1,20 @@
+//
+//  Cabinet.swift
+//  HistoryDigit
+//
+//  Created by Данил Менделев on 07.02.2023.
+//
+
+import SwiftUI
+
+struct Cabinet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Cabinet_Previews: PreviewProvider {
+    static var previews: some View {
+        Cabinet()
+    }
+}
