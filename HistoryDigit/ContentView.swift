d1c438c774859c5b7dd2a0025c75b6b24bf505e8
fDll37 <dmendelev@yandex.ru>
2023-02-07 19:31:28 +0300

    Initial Commit

---
+++ HistoryDigit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HistoryDigit
+//
+//  Created by Данил Менделев on 07.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HistoryDigit/HistoryDigitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryDigitApp.swift
+//  HistoryDigit
+//
+//  Created by Данил Менделев on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct HistoryDigitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
